932cf76d5efb6603cf645914a20079ba0c688cfb
Kamiko Loss <info@gloxi.net>
2026-01-15 09:02:19 +0900

    README に動画の準備方法を記載した

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ueatbeat
+# DRFINGER
 
+## 動画の準備方法
+
+動画をダウンロードする
+
+- https://github.com/yt-dlp/yt-dlp/wiki/Installation
+- `yt-dlp https://www.youtube.com/watch?v=Uk9-jvuVBPY -o input`
+
+動画を ogv に変換する
+
+- https://www.ffmpeg.org/download.html
+- https://docs.godotengine.org/ja/4.x/tutorials/animation/playing_videos.html
+- `ffmpeg -i input.webm -vf "scale=-1:720" -q:v 6 -q:a 6 -g:v 64 output.ogv`
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,20 +15,20 @@ var _keys_lane_lamp = {}
 
 
 func _ready() -> void:
-	_keys_lane_lamp = {
-		KEY_S: _lane_lamp_hat1,
-		KEY_D: _lane_lamp_snare1,
-		KEY_F: _lane_lamp_kick1,
-		KEY_J: _lane_lamp_kick2,
-		KEY_K: _lane_lamp_snare2,
-		KEY_L: _lane_lamp_hat2,
-	}
-	for key in _keys_lane_lamp.keys():
-		#print(_keys_lane_lamp[key])
-		_keys_lane_lamp[key].self_modulate = Color.TRANSPARENT
+    _keys_lane_lamp = {
+        KEY_S: _lane_lamp_hat1,
+        KEY_D: _lane_lamp_snare1,
+        KEY_F: _lane_lamp_kick1,
+        KEY_J: _lane_lamp_kick2,
+        KEY_K: _lane_lamp_snare2,
+        KEY_L: _lane_lamp_hat2,
+    }
+    for key in _keys_lane_lamp.keys():
+        #print(_keys_lane_lamp[key])
+        _keys_lane_lamp[key].self_modulate = Color.TRANSPARENT
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.keycode in _keys_lane_lamp.keys():
-			_keys_lane_lamp[event.keycode].self_modulate = Color.WHITE if event.pressed else Color.TRANSPARENT
+    if event is InputEventKey:
+        if event.keycode in _keys_lane_lamp.keys():
+            _keys_lane_lamp[event.keycode].self_modulate = Color.WHITE if event.pressed else Color.TRANSPARENT
