cefa38f8b42fd218e34302b5a031e5b2a9294764
Kamiko Loss <info@gloxi.net>
2026-01-17 18:45:04 +0900

    lane の塊を measure とした

--- scenes/editor/lane.gd
+++ scenes/editor/lane.gd
@@ -6,6 +6,7 @@ const LINE_COLOR_1 := Color(0.8, 0.8, 0.8)
 const LINE_COLOR_2 := Color(0.6, 0.6, 0.6)
 const LINE_COLOR_3 := Color(0.4, 0.4, 0.4)
 const LINE_COLOR_4 := Color(0.2, 0.2, 0.2)
+const LINE_COLOR_5 := Color(0.1, 0.1, 0.1)
 
 
 func _draw():
@@ -13,13 +14,15 @@ func _draw():
     var line_count := 16 # TODO
     var line_main_per := 4 # TODO
     for i in line_count:
-        var y := size.y * (line_count - i) / line_count
+        var y := size.y * i / line_count
         var from := Vector2(0, y)
         var to := Vector2(size.x, y)
-        var color := LINE_COLOR_4
-        if (line_count - i) % line_main_per == 0:
+        var color := LINE_COLOR_5
+        if i == 0:
             color = LINE_COLOR_3
+        elif i % line_main_per == 0:
+            color = LINE_COLOR_4
         draw_line(from, to, color)
     # 縦線 (左右の枠)
-    draw_line(Vector2(0, 0), Vector2(0, size.y), LINE_COLOR_2)
-    draw_line(Vector2(size.x, 0), Vector2(size.x, size.y), LINE_COLOR_2)
+    draw_line(Vector2(0, 0), Vector2(0, size.y), LINE_COLOR_3)
+    draw_line(Vector2(size.x, 0), Vector2(size.x, size.y), LINE_COLOR_3)
