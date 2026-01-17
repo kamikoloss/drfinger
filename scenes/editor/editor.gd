424d241d5b27d944e5b502dbdc11d00b3e2557f3
Kamiko Loss <info@gloxi.net>
2026-01-18 01:21:58 +0900

    lane -> measure -> editor に hover/press を伝えるようにした

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -25,6 +25,10 @@ const GRID_LABELS: Dictionary[GridType, String] = {
 }
 
 @export var _measure_scene: PackedScene
+@export var _note_scene: PackedScene
+
+var _current_measure: Measure = null
+var _current_lane: Measure = null
 
 @onready var _scroll_container: ScrollContainer = %ScrollContainer
 @onready var _measures_parent: Control = %MeasuresParent
@@ -34,19 +38,37 @@ const GRID_LABELS: Dictionary[GridType, String] = {
 
 
 func _ready() -> void:
-    # Measure 配置
-    var measure_count := 64
+    # Measure 初期化
+    var measure_count := 64 # TODO
     for i in (measure_count + 1):
         var measure: Measure = _measure_scene.instantiate()
         measure.index = measure_count - i
         measure.lane_types = LANE_TYPES
+        measure.hover_entered.connect(func(lane: Lane) -> void: _on_measure_hover_entered(measure, lane))
+        measure.hover_exited.connect(func(lane: Lane) -> void: _on_measure_hover_exited(measure, lane))
+        measure.pressed.connect(func(lane: Lane, pos: Vector2) -> void: _on_measure_pressed(measure, lane, pos))
         _measures_parent.add_child(measure)
-
     # スクロールを一番下へ
     #_scroll_container.set_deferred("scroll_vertical", _scroll_container.get_v_scroll_bar().max_value) # 動かん
     _scroll_container.set_deferred("scroll_vertical", 99999999)
 
-    # Grid
+    # UI - Grid
     for grid_type in GRID_LABELS:
         _option_button_grid.add_item(GRID_LABELS[grid_type], grid_type)
     _option_button_grid.selected = GridType.GRID_16 # TODO
+
+
+func _input(event: InputEvent) -> void:
+    pass
+
+
+func _on_measure_pressed(measure: Measure, lane: Lane, pos: Vector2) -> void:
+    print("[Editor] _on_measure_pressed() index: %s, lane_type: %s, pos: %s" % [measure.index, LaneType.Type.keys()[lane.lane_type], pos])
+
+
+func _on_measure_hover_entered(measure: Measure, lane: Lane) -> void:
+    print("[Editor] _on_measure_hover_entered()")
+
+
+func _on_measure_hover_exited(measure: Measure, lane: Lane) -> void:
+    print("[Editor] _on_measure_hover_exited()")
--- scenes/editor/lane.gd
+++ scenes/editor/lane.gd
@@ -1,6 +1,13 @@
 class_name Lane
 extends Control
 
+## ホバー開始時
+signal hover_entered()
+## ホバー終了時
+signal hover_exited()
+## クリック時
+signal pressed(pos: Vector2)
+
 const LINE_COLOR_1 := Color(0.8, 0.8, 0.8)
 const LINE_COLOR_2 := Color(0.6, 0.6, 0.6)
 const LINE_COLOR_3 := Color(0.4, 0.4, 0.4)
@@ -8,6 +15,30 @@ const LINE_COLOR_4 := Color(0.2, 0.2, 0.2)
 const LINE_COLOR_5 := Color(0.1, 0.1, 0.1)
 
 var lane_type := LaneType.Type.NONE
+
+## 現在ホバーしているかどうか
+var _hovered := false:
+    set(v):
+        #print("[Lane] hovered. (%s)" % [v])
+        _hovered = v
+        queue_redraw()
+        if _hovered:
+            hover_entered.emit()
+        else:
+            hover_exited.emit()
+
+
+func _ready() -> void:
+    gui_input.connect(_on_gui_input)
+    mouse_entered.connect(func() -> void: _hovered = true)
+    mouse_exited.connect(func() -> void: _hovered = false)
+
+
+func _on_gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton:
+        var event_mouse_button := event as InputEventMouseButton
+        if event_mouse_button.button_index == MOUSE_BUTTON_LEFT and event_mouse_button.pressed:
+            pressed.emit(event.position)
 
 
 func _draw():
@@ -30,3 +61,6 @@ func _draw():
     # 縦線 (左右の枠)
     draw_line(Vector2(0, 0), Vector2(0, size.y), LINE_COLOR_3)
     draw_line(Vector2(size.x, 0), Vector2(size.x, size.y), LINE_COLOR_3)
+    # 前景
+    if _hovered:
+        draw_rect(Rect2(Vector2.ZERO, size), Color(Color.WHITE, 0.1))
