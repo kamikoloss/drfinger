f51ac53e7ba7e5fbd2fa80c96324406aa0f46b40
Kamiko Loss <info@gloxi.net>
2026-01-18 17:44:07 +0900

    とりあえず Editor 内に FileDialogue を置いた

--- scenes/editor/editor.gd
+++ scenes/editor/editor.gd
@@ -14,6 +14,7 @@ const CHART_FILE_NAME := "chart.json"
 @onready var _button_save: Button = %ButtonSave
 @onready var _button_play: Button = %ButtonPlay
 @onready var _button_stop: Button = %ButtonStop
+@onready var _file_dialog: FileDialog = %FileDialog
 
 
 func _ready() -> void:
