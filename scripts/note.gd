14698dc174bda80f74cb5ea59e4b6862525b21ba
Kamiko Loss <info@gloxi.net>
2026-01-24 15:40:08 +0900

    chore

--- scripts/note.gd
+++ scripts/note.gd
@@ -3,6 +3,7 @@ extends RefCounted
 
 var lane_type := LaneType.Type.NONE
 var tick := 0
+# TODO: Velocity
 
 
 func _init(lane_type_: String, tick_: int) -> void:
@@ -12,10 +13,10 @@ func _init(lane_type_: String, tick_: int) -> void:
 
 func serialize() -> Dictionary:
     return {
-        "lt": lane_type,
-        "ti": tick,
+        "l": lane_type,
+        "t": tick,
     }
 
 
 static func deserialize(dict: Dictionary) -> Note:
-    return new(dict["lt"], dict["ti"])
+    return new(dict["l"], dict["t"])
